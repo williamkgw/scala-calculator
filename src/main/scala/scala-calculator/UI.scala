a2cd1062b9a8b57c2314a328ad3a8da579ca009f
William Kennedy <williamkennedyg7@gmail.com>
2026-07-28 18:26:07 -0300

    add Display integration with compiler.

--- src/main/scala/scala-calculator/UI.scala
+++ src/main/scala/scala-calculator/UI.scala
@@ -21,16 +21,28 @@ class UIButton(buttonText: String, eventButton: EventBus[String]) extends Rendab
 class UIDisplay(eventButton: EventBus[String]) extends Rendable {
   import ExpressionOperations.*
 
-  def calculateDisplayText(displayText: Signal[String]) =
-    displayText.map {
-      text =>
-        val tokens = Tokenizer.tokenize(text)
+  def updateDisplay(display: String, button: String): String =
+    button match {
+      case "DEL" =>
+        display.substring(0, display.size - 1)
+
+      case "=" =>
+        val tokens = Tokenizer.tokenize(display)
         val (tree, rest) = AbstractSyntaxTreeParser.parseExpression(tokens)
         Evaluator.evaluate(tree).toString
+
+      case "ON" =>
+        display
+
+      case value =>
+        display + value
     }
 
   override def render(): HtmlElement =
-    val displayText = eventButton.events.scanLeft("")(_ + _)
+    val displayText = eventButton.events.scanLeft("")(
+        (display, button) => updateDisplay(display, button)
+    )
+
     tr(
       td(
         colSpan := 4,
